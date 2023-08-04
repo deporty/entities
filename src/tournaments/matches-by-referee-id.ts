769388d00c0ea154e66c8c16c09b348f3f71222b
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-08-04 16:13:28 -0500

    Clean matches-by-referee-id file

--- src/tournaments/matches-by-referee-id.ts
+++ src/tournaments/matches-by-referee-id.ts
@@ -1,22 +1,24 @@
-import { Entity, Id } from "../general";
+import { Entity, Id } from '../general';
 export type KindMatch = 'main-draw' | 'intergroup' | 'group';
 
 export interface MainDrawMatchesByRefereeIdEntity extends Entity {
-    kind: 'main-draw',
-    refereeId: Id,
-    tournamentId: Id,
-    matchId: Id,
+  kind: 'main-draw';
+  refereeId: Id;
+  tournamentId: Id;
+  matchId: Id;
 }
 
-export interface IntergroupMatchesByRefereeIdEntity extends Omit<MainDrawMatchesByRefereeIdEntity,'kind'> {
-    kind: 'intergroup',
-    fixtureStageId: Id,
-
+export interface IntergroupMatchesByRefereeIdEntity extends Omit<MainDrawMatchesByRefereeIdEntity, 'kind'> {
+  kind: 'intergroup';
+  fixtureStageId: Id;
 }
 
-export interface GroupMatchesByRefereeIdEntity extends Omit<MainDrawMatchesByRefereeIdEntity,'kind'> {
-    kind: 'group',
-    fixtureStageId: Id,
-    groupId: Id,
+export interface GroupMatchesByRefereeIdEntity extends Omit<MainDrawMatchesByRefereeIdEntity, 'kind'> {
+  kind: 'group';
+  fixtureStageId: Id;
+  groupId: Id;
 }
-export type MatchesByRefereeIdEntity = GroupMatchesByRefereeIdEntity | IntergroupMatchesByRefereeIdEntity | MainDrawMatchesByRefereeIdEntity;
+export type MatchesByRefereeIdEntity =
+  | GroupMatchesByRefereeIdEntity
+  | IntergroupMatchesByRefereeIdEntity
+  | MainDrawMatchesByRefereeIdEntity;
