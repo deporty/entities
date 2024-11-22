4fe7c78172f95571577845988f6f71b598aa8808
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2024-11-22 13:20:11 -0500

    Update

--- src/general/general.ts
+++ src/general/general.ts
@@ -1,6 +1,12 @@
+import { v4 } from 'uuid';
+
 export type Id = string;
 export type FilePath = string;
 
 export interface Entity {
-  id?: Id;
+	id?: Id;
+}
+
+export function ID() {
+	return v4().replace(/[-]/g, '').substring(0, 20);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-export * from './players/index';
-export * from './teams/index';
-export * from './users/index';
+export * from './administration/index';
+export * from './authorization/index';
 export * from './general/index';
 export * from './invoices/index';
 export * from './locations/index';
-export * from './tournaments/index';
-export * from './authorization/index';
 export * from './news/index';
-export * from './administration/index';
+export * from './players/index';
+export * from './teams/index';
+export * from './tournaments/index';
+export * from './users/index';
+
